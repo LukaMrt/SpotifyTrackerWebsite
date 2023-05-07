359480581caca2b314bc67508286370a07fde25d
Luka Maret <maret.luka@gmail.com>
2023-05-07 22:27:20 +0200

    🧹 chore(app): setup client

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,14 @@
+import type {Component} from "solid-js";
+
+const App: Component = () => {
+
+    const test = 1;
+
+    return (
+        <>
+            <p>{test}</p>
+        </>
+    );
+};
+
+export default App;
